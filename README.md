6f0de22f4333e269cea67689d899140d883c9abf
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-10-09 14:41:59 +0200

    First setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Apache Cordova tutorial
+
+
---
+++ www/js/main.js
@@ -0,0 +1,17 @@
+
+
+// launched when cordova ready
+document.addEventListener('deviceReady', function() {
+  console.log("Device is ready yeaaaaa !")
+}, false);
+
+// application main module
+var app = angular.module('app', ['ngRoute']);
+
+// routes and views
+app.config(function($routeProvider) {
+  $routeProvider
+      .when('/home', {templateUrl : 'partials/home.html'})
+      .when('/about', {templateUrl : 'partials/about.html'})
+      .otherwise({redirectTo : '/home'});
+});
